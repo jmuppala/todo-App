179ceb5dc9686db56b57c2b54c71325fc93f548f
Jogesh Muppala <jmuppala@hotmail.com>
2020-07-13 13:53:51 +0800

    useReducer Hook

--- src/State/Todos.js
+++ src/State/Todos.js
@@ -1,24 +1,44 @@
-import { useState } from 'react';
+import * as ActionTypes from './ActionTypes';
+import { useReducer } from 'react';
+
+function reducer (state, action) {
+    switch(action.type) {
+        case ActionTypes.ADD_TODO:
+            return state.concat({_id: Date.now(), title: action.payload, completed: false});
+
+        case ActionTypes.TOGGLE_TODO:
+            return state.map((todo) => {
+                if (todo._id === action.payload)
+                    todo.completed = !todo.completed;
+                return todo;
+            });
+
+        case ActionTypes.DELETE_TODO:
+            return state.filter((todo) => todo._id !== action.payload);
+
+        default:
+            return state;
+    }
+}
 
 export default function useTodos() {
 
-    const [todos, setTodos] = useState([]);
-      
-    const addTodo = (todoTitle) => {
-      setTodos(todos.concat({_id: Date.now(), title: todoTitle, completed: false}))
-    };
-  
-    const toggleTodo = (todoId) => {
-      setTodos(todos.map((todo) => {
-        if (todo._id === todoId)
-            todo.completed = !todo.completed;
-        return todo;
-      }));
-    };
-  
-    const deleteTodo = (todoId) => {
-      setTodos(todos.filter((todo) => todo._id !== todoId))
-    };
+    const [todos, dispatch] = useReducer(reducer, []);
     
+    const addTodo = (todoTitle) => dispatch({
+        type: ActionTypes.ADD_TODO,
+        payload: todoTitle
+    });
+    
+    const toggleTodo = (todoId) => dispatch({
+        type: ActionTypes.TOGGLE_TODO,
+        payload: todoId
+    });
+    
+    const deleteTodo = (todoId) => dispatch({
+        type: ActionTypes.DELETE_TODO,
+        payload: todoId
+    });
+
     return [todos, addTodo, toggleTodo, deleteTodo];
 }
